da6eb840aa7bccc0d20d5097d5ad6b1fc3b1a2a6
mztikk <nevigskcuforez@gmail.com>
2020-11-05 20:08:06 +0100

    added models proj

---
+++ Models/Records.cs
@@ -0,0 +1,12 @@
+﻿namespace Models
+{
+    public record Person
+    {
+        public string FirstName { get; init; }
+        public string LastName { get; init; }
+    }
+
+    public record Teacher : Person { }
+    public record Student : Person { }
+
+}
